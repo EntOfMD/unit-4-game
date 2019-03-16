f6b3331374c53f0adb25f54c26cb6cc3913eab37
Ashell Fernando <ashellz2010@gmail.com>
2019-03-16 13:53:24 -0400

    starting the value input equation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,21 @@
+'use strict';
+
+//it's too damn organized, DRY DRY DRY!!!
+
 //values that updates
 let values = {
     wins: 0,
     losses: 0,
     current_value: 0,
-    gameName: [],
-    titleArr: []
+    titleArr: [],
+    goal: 0
 };
 
 //DOM iDs
 let DOMs = {
     //Title
     game_name: 'Crystal  Collector',
+
     //paragraphs
     title: $('#game-title'),
     midP: $('#motiWords'),
@@ -18,24 +23,43 @@ let DOMs = {
     //scores
     wins: $('#wins_ui'),
     losses: $('#losses_ui'),
-    totalScore: $('#total_score'),
+    goal: $('#total_score'),
     currentValue: $('#current_value'),
 
     //crystals
-    red: $('#red_crystal'),
-    blue: $('#blue_crystal'),
-    yellow: $('#yellow_crystal'),
-    green: $('#green_crystal')
+    red: $('#red_btn'),
+    blue: $('#blue_btn'),
+    yellow: $('#yellow_btn'),
+    green: $('#green_btn')
 };
 
 //functions
 var fx = {
+    //setting the DOMs
+    appInit: () => {
+        fx.changeRandomColor(); //changes color
+        fx.playerStartGoal(); //sets random goal
+        console.log(`Current Goal to beat: ${fx.playerStartGoal()}`);
+        DOMs.wins.append(values.wins); //sets wins
+        DOMs.losses.append(values.losses); //sets losses
+        //not supposed to show values until user clicks the crystal
+        if (values.current_value === 0) {
+            DOMs.currentValue
+                .addClass('small')
+                .append('Click a crystal to start');
+        } else {
+            DOMs.currentValue.append(values.current_value);
+        }
+        //this is the same as above's if statement, but using ternary operator
+        // condition ? true : else
+        values.goal === 0
+            ? DOMs.goal.addClass('small').append('Click a crystal to start')
+            : DOMs.goal.append(values.goal);
+    },
     //stores each letter individually, so we can manipulate it later
     getTitle: () => {
         for (let i = 0; i < DOMs.game_name.length; i++) {
-            //try to change the color of each letter
             values.titleArr.push(DOMs.game_name[i]);
-            console.log(`Pushed letter ${values.titleArr[i]}`);
         }
     },
     //I wanted to change color of individual letter, but that'll do pig, that'll do.
@@ -43,20 +67,75 @@ var fx = {
     changeRandomColor: () => {
         fx.getTitle();
         var colors = ['#8e0000', '#0866cc', '#d6cf04', '#0fc100']; //red, blue, yellow, green
+        let random = fx.randomNum(colors.length);
+
+        switch (random) {
+            case 0:
+                console.log(`Title color: red`);
+                break;
+
+            case 1:
+                console.log(`Title color: blue`);
+                break;
+
+            case 2:
+                console.log(`Title color: yellow`);
+                break;
+
+            case 3:
+                console.log(`Title color: green`);
+                break;
+
+            default:
+                console.log(random);
+                break;
+        }
+
         values.titleArr.map(letter => {
-            let random = Math.floor(Math.random() * colors.length);
-            if (random == random) {
-                Math.floor(Math.random() * colors.length);
-            }
-            $('#game-title')
+            $(DOMs.title)
                 .css('color', colors[random])
                 .append(letter);
         });
+    },
+
+    randomNum: p => {
+        let generatedNumber = Math.floor(Math.random() * p);
+
+        return generatedNumber;
+    },
+
+    playerStartGoal: () => {
+        let goal = fx.randomNum(100);
+        values.goal = goal;
+        return goal;
     }
 };
 
 //this is a shorthand version of $(document).ready
 //shit goes down from here on, top part is preparing for the fight~~
 $(function() {
-    fx.changeRandomColor();
+    fx.appInit();
+    // $(DOMs.red).click
+    let red_value = values.goal / 4;
+    let blue_value = values.goal / 5;
+    let yellow_value = values.goal / 6;
+    let green_value = Math.floor(yellow_value / 2) + 1 * 5;
+
+    $(DOMs.red).click(function(e) {
+        e.preventDefault();
+
+        console.log(red_value);
+    });
+    $(DOMs.blue).click(function(e) {
+        e.preventDefault();
+        console.log(blue_value);
+    });
+    $(DOMs.yellow).click(function(e) {
+        e.preventDefault();
+        console.log(yellow_value);
+    });
+    $(DOMs.green).click(function(e) {
+        e.preventDefault();
+        console.log(green_value);
+    });
 });
